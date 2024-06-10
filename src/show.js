ddbd0f2c79ff1b950d16e04e6ea9200ee524a6be
LautaTuzzzio <aerodactyl2006@gmail.com>
2024-06-10 08:35:47 -0300

    recycle bin finished

--- src/show.js
+++ src/show.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var recycleBinImage = document.getElementById("recycleBinImage");
     var recycleBinHolder = document.getElementById("recycleBinHolder");
     var index3Content = document.getElementById("index3Content");
+    var closeButton = document.querySelector(".close-ico");
 
     if (recycleBinImage) {
         recycleBinImage.addEventListener("click", toggleIndexContent);
@@ -11,12 +12,48 @@ document.addEventListener("DOMContentLoaded", function() {
         recycleBinHolder.addEventListener("click", toggleIndexContent);
     }
 
+    if (closeButton) {
+        closeButton.addEventListener("click", closeIndexContent);
+    }
+
     function toggleIndexContent() {
         if (index3Content.style.display === "none" || index3Content.style.display === "") {
             index3Content.style.display = "block";
             index3Content.style.position = "absolute";
         }
     }
+
+    function closeIndexContent() {
+        index3Content.style.display = "none";
+    }
 });
 
-    
+document.addEventListener("DOMContentLoaded", function() {
+    var recycleBinImage = document.getElementById("internetExplorerImage");
+    var recycleBinHolder = document.getElementById("internetExplorerHolder");
+    var index3Content = document.getElementById("index2Content");
+    var closeButton = document.querySelector(".close-ico");
+
+    if (recycleBinImage) {
+        recycleBinImage.addEventListener("click", toggleIndexContent);
+    }
+
+    if (recycleBinHolder) {
+        recycleBinHolder.addEventListener("click", toggleIndexContent);
+    }
+
+    if (closeButton) {
+        closeButton.addEventListener("click", closeIndexContent);
+    }
+
+    function toggleIndexContent() {
+        if (index2Content.style.display === "none" || index2Content.style.display === "") {
+            index2Content.style.display = "block";
+            index2Content.style.position = "absolute";
+        }
+    }
+
+    function closeIndexContent() {
+        index2Content.style.display = "none";
+    }
+});
